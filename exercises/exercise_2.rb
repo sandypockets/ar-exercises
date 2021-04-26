e72fa63786067489a4a5c1ac01301326ef94c116
Zac Alexander <zac@znacreative.com>
2021-04-26 20:57:25 +0000

    Set up second exercise

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,12 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+# Load the first store (with id = 1) from the database and assign it to an instance variable @store1.
+@store1 = Store.find_by(id: 1)
+# Load the second store from the database and assign it to @store2.
+@store2 = Store.find_by(id: 2)
+# Update the first store (@store1) instance in the database. (Change its name or something.)
+@store1.update(name: 'The Blue Mountains')
+# Confirm the name has changed
+puts @store1.name
+
