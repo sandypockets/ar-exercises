09a6e56a0d1792460438af32b5198b840f33ceeb
Zac Alexander <zac@znacreative.com>
2021-04-26 21:36:53 +0000

    Outline validation requirements

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ActiveRecord::Base
 
 end
+
+# Validation requirements
+# # Employees must always have a first name present
+# # Employees must always have a last name present
+# # Employees have a hourly_rate that is a number (integer) between 40 and 200
+# # Employees must always have a store that they belong to (can't have an employee that is not assigned a store)
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,8 @@
 class Store < ActiveRecord::Base
 
 end
+
+# Validation requirements
+# # Stores must always have a name that is a minimum of 3 characters
+# # Stores have an annual_revenue that is a number (integer) that must be 0 or more
+# # BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a custom validation method - don't use a Validator class)
